0516a23698b764a38109693d542f6d3c20f85bab
mariangr <marian_graurov@abv.bg>
2014-09-13 01:43:12 +0300

    changed style, added 404 not found page, added glimpse for performance statistics

--- IExam/Controllers/HomeController.cs
+++ IExam/Controllers/HomeController.cs
@@ -17,6 +17,5 @@ namespace IExam.Controllers
         {
             return PartialView(pageName);
         }
-
 	}
 }
---
+++ IExam/Controllers/StatusCodesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IExam.Controllers
+{
+    public class StatusCodesController : Controller
+    {
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        public ActionResult OK()
+        {
+            return View();
+        }
+
+
+	}
+}

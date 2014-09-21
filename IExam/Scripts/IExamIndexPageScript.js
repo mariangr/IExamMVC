0cd46087aebef3b07f99ca721c024d991a51f846
mariangr <marian_graurov@abv.bg>
2014-09-21 14:58:15 +0300

    refactoring

---
+++ IExam/Scripts/IExamIndexPageScript.js
@@ -0,0 +1,27 @@
+﻿var IExamIndex = IExamIndex || {};
+
+$(document).ready(function () {
+    IExamIndex.PageLogic.loadSelectedPage('/Home/ReturnPartialView/', '_Home');
+});
+
+IExamIndex.PageLogic = function () {
+    var loadSelectedPage = function (url, page) {
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: { pageName: page },
+            success: function (html) {
+                $("#main_Container_Div").html(html);
+            },
+            error: function () {
+                $('#main_Container_Div').html("Error");
+            }
+        })
+        return false;
+    }
+
+    return {
+        loadSelectedPage: loadSelectedPage
+
+    }
+}();
--- IExam/Scripts/iExamVideoPageScript.js
+++ IExam/Scripts/iExamVideoPageScript.js
@@ -1,69 +1,24 @@
-﻿$(document).ready(function () {
-    loadSelectedPage('_Home');
-});
+﻿var IExamVideo = IExamVideo || {};
 
-function loadSelectedPage(page) {
-    $.ajax({
-        type: 'POST',
-        url: '/Home/ReturnPartialView/',
-        data: { pageName: page },
-        success: function (html) {
-            $("#main_Container_Div").html(html);
-        },
-        error: function () {
-            $('#main_Container_Div').html("Error");
+IExamVideo.PageLogic = function () {
+
+    var manageLink = function() {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var link = $('#link').val();
+        var match = link.match(regExp);
+        if (match && match[2].length == 11) {
+            link = match[2];
         }
-    })
-    return false;
-}
-
-//var VIDEO_RESPONSE = {
-//    "SUCCESS": "Video Successfully added!",
-//    "DUPLICATE": "Video already exists",
-//    "FAILURE": "Service error!"
-//}
-
-function manageLink() {
-    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    var link = $('#link').val();
-    var match = link.match(regExp);
-    if (match && match[2].length == 11) {
-        link = match[2];
+        $('#link').val(link);
     }
-    $('#link').val(link);
-}
 
-function loadVideosList() {
-    $.ajax({
-        type: 'GET',
-        url: '/Video/GetVideoElements/',
-        success: function (result) {
-            $('#videosContainer').html(result);
-
-        },
-        error: function (result) {
-            alert(result.statusText)
-        }
-    })
-    return false;
-
-}
-
-function LoadVideo(id) {
-    if (id != null) {
+    var loadVideosList = function() {
         $.ajax({
             type: 'GET',
-            url: '/Video/GetVideoPlayer/',
-            data: { id: id },
+            url: '/Video/GetVideoElements/',
             success: function (result) {
-                $('#videosPlayerContainer').html(result);
-                getCurrentVideoComments();
+                $('#videosContainer').html(result);
 
-                $('textarea').width($('#videoComents').width());
-                $('#videosPlayerContainer').show();
-                $('iframe').width($('#videosPlayerContainer').width());
-                $('iframe').height($('iframe').width() * 3 / 4);
-                $('#videoComents').width($('iframe').width());
             },
             error: function (result) {
                 alert(result.statusText)
@@ -71,104 +26,135 @@ function LoadVideo(id) {
         })
         return false;
     }
-    else {
-        $('#videosPlayerContainer').html("");
+
+    var LoadVideo = function(id) {
+        if (id != null) {
+            $.ajax({
+                type: 'GET',
+                url: '/Video/GetVideoPlayer/',
+                data: { id: id },
+                success: function (result) {
+                    $('#videosPlayerContainer').html(result);
+                    getCurrentVideoComments();
+
+                    $('textarea').width($('#videoComents').width());
+                    $('#videosPlayerContainer').show();
+                    $('iframe').width($('#videosPlayerContainer').width());
+                    $('iframe').height($('iframe').width() * 3 / 4);
+                    $('#videoComents').width($('iframe').width());
+                },
+                error: function (result) {
+                    alert(result.statusText)
+                }
+            })
+            return false;
+        }
+        else {
+            $('#videosPlayerContainer').html("");
+        }
     }
-}
 
-function DeleteVideo(id) {
-    $.ajax({
-        type: 'GET',
-        url: '/Video/DeleteVideo/',
-        data: { id: id },
-        success: function (result) {
-            LoadVideo(null);
-            $('#videoComents').html("");
-            loadVideosList();
-            $('#videosPlayerContainer').hide();
+    var DeleteVideo = function DeleteVideo(id) {
+        $.ajax({
+            type: 'GET',
+            url: '/Video/DeleteVideo/',
+            data: { id: id },
+            success: function (result) {
+                LoadVideo(null);
+                $('#videoComents').html("");
+                loadVideosList();
+                $('#videosPlayerContainer').hide();
 
-        },
-        error: function (result) {
-            alert(result.statusText)
-        }
-    })
-    return false;
-}
+            },
+            error: function (result) {
+                alert(result.statusText)
+            }
+        })
+        return false;
+    }
 
-function getCurrentVideoComments() {
+    var getCurrentVideoComments = function() {
+        var id = $('#selectedVideoInPlayerID').val();
+        var link = $('#selectedVideoInPlayerLink').val();
+        $.ajax({
+            type: 'GET',
+            url: '/Video/GetVideoComments/',
+            data: { id: id, link: link },
+            success: function (result) {
+                $('#videoComents').html(result);
+            },
+            error: function (result) {
+                alert(result.statusText)
+            }
+        })
+        return false;
+    }
 
-    var id = $('#selectedVideoInPlayerID').val();
-    var link = $('#selectedVideoInPlayerLink').val();
-    $.ajax({
-        type: 'GET',
-        url: '/Video/GetVideoComments/',
-        data: { id: id, link: link },
-        success: function (result) {
-            $('#videoComents').html(result);
-        },
-        error: function (result) {
-            alert(result.statusText)
-        }
-    })
-    return false;
-
-}
-
-function SendComment() {
-    var linkOfComment = $('#selectedVideoInPlayerLink').val();
-    var message = $('#commentForVideo').val();
-    var user = $('#currentUser').val();
-    $.ajax({
-        type: 'GET',
-        url: '/Video/CreateVideoComment/',
-        data: { link: linkOfComment, message: message, user: user },
-        success: function (result) {
-            if (result == "CommentAddedSuccessfully") {
+    var deleteComment = function(id) {
+        $.ajax({
+            type: 'GET',
+            url: '/Video/DeleteVideoComment/',
+            data: { id: id },
+            success: function (result) {
                 getCurrentVideoComments();
+            },
+            error: function (result) {
+                alert(result.statusText)
             }
-            else if (result == "EmptyComment") {
-                $('#commentForVideo').attr('placeholder', "Please write a comment...");
-            }
-            else if (result == "UnspecifiedException") {
-                alert('Server Error');
-            }
-
-        },
-        error: function (result) {
-            alert(result.statusText)
-        }
-    })
-    return false;
-
-
-}
-
-function DeleteComment(id) {
-    $.ajax({
-        type: 'GET',
-        url: '/Video/DeleteVideoComment/',
-        data: { id: id },
-        success: function (result) {
-            getCurrentVideoComments();
-        },
-        error: function (result) {
-            alert(result.statusText)
-        }
-    })
-    return false;
-
-}
-
-function textAreaEnterEvent(e) {
-    
-    if (e.which == 13) {
-        SendComment();
+        })
+        return false;
     }
-    else {
-        return;
-    }
-}
 
-function countDown() {
-    $('.countdown').html('1000' - $('#commentForVideo').val().length + " remaining symbols");
-}
+    var sendComment = function () {
+        var linkOfComment = $('#selectedVideoInPlayerLink').val();
+        var message = $('#commentForVideo').val();
+        var user = $('#currentUser').val();
+        $.ajax({
+            type: 'GET',
+            url: '/Video/CreateVideoComment/',
+            data: { link: linkOfComment, message: message, user: user },
+            success: function (result) {
+                if (result == "CommentAddedSuccessfully") {
+                    getCurrentVideoComments();
+                }
+                else if (result == "EmptyComment") {
+                    $('#commentForVideo').attr('placeholder', "Please write a comment...");
+                }
+                else if (result == "UnspecifiedException") {
+                    alert('Server Error');
+                }
+
+            },
+            error: function (result) {
+                alert(result.statusText)
+            }
+        })
+        return false;
+    }
+
+    var textAreaEnterEvent = function(e) {
+
+        if (e.which == 13) {
+            sendComment();
+        }
+        else {
+            return;
+        }
+    }
+
+    var countDown = function() {
+        $('.countdown').html('1000' - $('#commentForVideo').val().length + " remaining symbols");
+    }
+
+    return {
+        manageLink: manageLink,
+        loadVideosList: loadVideosList,
+        LoadVideo: LoadVideo,
+        DeleteVideo: DeleteVideo,
+        deleteComment: deleteComment,
+        sendComment: sendComment,
+        textAreaEnterEvent: textAreaEnterEvent,
+        countDown: countDown
+    }
+
+}();

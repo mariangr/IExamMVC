f95f4495680624255e01d224fd6a6e46bf4a0965
mariangr <marian_graurov@abv.bg>
2014-09-22 00:24:52 +0300

    Added foreign key constraint between tests and questions

---
+++ IExam/Controllers/TestsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IExam.Models;
+
+namespace IExam.Controllers
+{
+    [Authorize]
+    public class TestsController : Controller
+    {
+        private TestsDBContext testDB = new TestsDBContext();
+        //
+        // GET: /Tests/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetTestData()
+        {
+            return Json(testDB.Tests.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+
+        public void DeleteTest(int id)
+        {
+            Test test = testDB.Tests.Find(id);
+            var TestQuestions = testDB.Questions.Where(q => q.TestID == id);
+            foreach(var question in TestQuestions)
+            {
+                testDB.Questions.Remove(question);
+            }
+            testDB.Tests.Remove(test);
+            testDB.SaveChanges();
+        }
+
+        public void CreateTest(string name) {
+            Test newTest = new Test();
+            int index = 0;
+            try
+            {
+                index = (int)(testDB.Tests.Max(t => t.TestID) + 1);
+            }
+            catch (InvalidOperationException)
+            { 
+                
+            }
+            newTest.TestName = name;
+            newTest.TestID = index;
+            testDB.Tests.Add(newTest);
+            testDB.SaveChanges();
+        }
+	}
+}
---
+++ IExam/Scripts/IExamTestsIndexPageScript.js
@@ -0,0 +1,90 @@
+﻿var IExamIndex = IExamIndex || {};
+
+$(document).ready(function () {
+    IExamIndex.Tests = new IExamIndex.TestsModel();
+    ko.applyBindings(IExamIndex.Tests, document.getElementById(test_data))
+    IExamIndex.PageLogic.FillTestTable();
+    $('#requirmentName').hide();
+})
+
+IExamIndex.TestsModel = function () {
+    var self = this;
+
+    self.tests = ko.observableArray([]);
+
+}
+
+IExamIndex.TestVM = function (id, name) {
+    var self = this;
+
+    self.id = ko.observable(id);
+    self.name = ko.observable(name);
+}
+
+IExamIndex.PageLogic = function () {
+    var DeleteTest = function (event) {
+        var id = $(event.target).attr('id');
+        $.ajax({
+            type: 'POST',
+            url: '/Tests/DeleteTest/',
+            data: {id: id},
+            success: function (success) {
+                FillTestTable();
+            },
+            error: function () {
+                alert('error');
+            }
+        })
+    }
+
+    var FillTestTable = function () {
+        $.ajax({
+            type: 'GET',
+            url: '/Tests/GetTestData/',
+            data: JSON,
+            success: function (tableResult) {
+                var tempArray = [];
+                for (var userI in tableResult) {
+                    tempArray.push(new IExamIndex.TestVM(
+                        tableResult[userI].TestID,
+                        tableResult[userI].TestName
+                        ))
+                }
+                IExamIndex.Tests.tests.removeAll();
+                ko.utils.arrayPushAll(IExamIndex.Tests.tests(), tempArray);
+                IExamIndex.Tests.tests.valueHasMutated();
+            },
+            error: function () {
+                alert('error');
+            }
+        })
+    }
+
+    var CreateTest = function () {
+        var name = $('#testName').val();
+        if (name.length > 0) {
+            $.ajax({
+                type: 'POST',
+                url: '/Tests/CreateTest/',
+                data: { name: name },
+                success: function () {
+                    $('#testName').val("");
+                    FillTestTable();
+                },
+                error: function (result) {
+                    alert(result);
+                }
+            })
+        }
+        else {
+            $('#requirmentName').fadeIn(3000).fadeOut(1000);
+        }
+    }
+
+    return {
+        DeleteTest: DeleteTest,
+        FillTestTable: FillTestTable,
+        CreateTest: CreateTest
+        
+    }
+}();

5c4ee9986003e7bd866dcf50daabf07621e4f7dd
mariangr <marian_graurov@abv.bg>
2014-10-03 22:13:00 +0300

    fixed new question validation

--- IExam/Scripts/PagesScript/Tests/IExamTestQuestionsPageScript.js
+++ IExam/Scripts/PagesScript/Tests/IExamTestQuestionsPageScript.js
@@ -26,23 +26,25 @@ IExamTestQuestion.PageLogic = function () {
         var third = $('#newTestThirdAnswer' + id).val();
         var fourth = $('#newTestFourthAnswer' + id).val();
         var answer = $('#newTestAnswer' + id).val();
-        $.ajax({
-            type: 'POST',
-            url: '/Tests/AddTestQuestions/',
-            data: { TestID: id, QuestionDescription: question, FirstAnswer: first, SecondAnswer: second, ThirdAnswer: third, FourthAnswer: fourth, Answer: answer },
-            success: function (result) {
-                IExamTestIndex.PageLogic.ShowQestions(null, id);
-                $('#newTestQuestion' + id).val("");
-                $('#newTestFirstAnswer' + id).val("");
-                $('#newTestSecondAnswer' + id).val("");
-                $('#newTestThirdAnswer' + id).val("");
-                $('#newTestFourthAnswer' + id).val("");
-                $('#newTestAnswer' + id).val("1");
-            },
-            error: function (result) {
-                alert(result);
-            }
-        })
+        if (question != "" && first != "" && second != "" && third != "" && fourth != "") {
+            $.ajax({
+                type: 'POST',
+                url: '/Tests/AddTestQuestions/',
+                data: { TestID: id, QuestionDescription: question, FirstAnswer: first, SecondAnswer: second, ThirdAnswer: third, FourthAnswer: fourth, Answer: answer },
+                success: function (result) {
+                    IExamTestIndex.PageLogic.ShowQestions(null, id);
+                    $('#newTestQuestion' + id).val("");
+                    $('#newTestFirstAnswer' + id).val("");
+                    $('#newTestSecondAnswer' + id).val("");
+                    $('#newTestThirdAnswer' + id).val("");
+                    $('#newTestFourthAnswer' + id).val("");
+                    $('#newTestAnswer' + id).val("1");
+                },
+                error: function (result) {
+                    alert(result);
+                }
+            })
+        }
     }
 
     return {

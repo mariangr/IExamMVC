e99796ac32df7f1fa3208968cb6802fee4122b65
Marian Graurov <marian_graurov@abv.bg>
2015-03-17 22:34:14 +0200

    fixed chat for unregistered users

--- IExam/Scripts/main.js
+++ IExam/Scripts/main.js
@@ -19,44 +19,6 @@ var sliderManager = function () {
     }
 }()
 
-
-$(function () {
-    var chat = $.connection.chatHub;
-
-    chat.client.addNewMessageToPage = function (name, message) {
-        if (!$('#chatContainer').hasClass('open')) {
-            $('#showChatButton').removeClass('btn-info').addClass('btn-danger');
-        }
-        $('#discussion').append('<li><strong>' + htmlEncode(name)
-            + '</strong>: ' + htmlEncode(message) + '</li>');
-
-        $('.chatMessages').scrollTop(Infinity);
-    };
-
-    $('#message').focus();
-
-    $.connection.hub.start().done(function () {
-        $('#sendmessage').click(function () {
-            if ($('#message').val().length > 0) {
-                chat.server.send($('#displayname').val(), $('#message').val().trim());
-                $('#message').val('').focus();
-            }
-        });
-
-        $('#message').on('keydown', function (e) {
-            if (e.which == 13) {
-                if ($('#message').val().length > 0) {
-                    chat.server.send($('#displayname').val(), $('#message').val().trim());
-                    $('#message').val('').focus();
-                }
-            }
-            else {
-                return;
-            }
-        })
-    });
-});
-
 function htmlEncode(value) {
     var encodedValue = $('<div />').text(value).html();
     return encodedValue;
